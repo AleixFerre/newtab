44fe5f0f21dbcd65cb8dd5c4611efc89e5646bba
Aleix Ferré <thecatalahd@gmail.com>
2022-04-28 21:02:53 +0200

    Fix: Removing unused random function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,4 @@ export class AppComponent implements OnInit {
     });
     this.backgroundStyle = `url(${environment.root}assets/backgrounds/1.jpg)`;
   }
-
-  // Returns a random number between 1 and n
-  getRandom(n: number): number {
-    return Math.floor(Math.random() * n) + 1;
-  }
 }
